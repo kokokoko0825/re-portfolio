273cbf1e533484301a845c770a347780763c0d1b
kokokoko0825 <koushi.tanaka54@gmail.com>
2025-06-20 13:26:24 +0900

    feat: HeaderとMobileMenuにメディアクエリを追加し、ハイドレーション後の表示制御を実装

--- app/component/Header/Header.tsx
+++ app/component/Header/Header.tsx
@@ -3,9 +3,17 @@ import * as styles from "./styles.css";
 import { Link } from "@remix-run/react";
 import { useMenu } from "../../contexts/MenuContext";
 import { MobileMenu } from "../MobileMenu/MobileMenu";
+import { useIsMobile, useIsClient } from "../../hooks/useMediaQuery";
 
 export function Header(): ReactNode {
     const { toggleMenu } = useMenu();
+    const isMobile = useIsMobile();
+    const isClient = useIsClient();
+
+    // ハイドレーション完了前は従来のCSSメディアクエリに依存
+    // ハイドレーション完了後はJavaScriptでの判定を使用
+    const showDesktopMenu = isClient ? !isMobile : true;
+    const showMobileButton = isClient ? isMobile : true;
 
     return (
         <>
@@ -15,14 +23,27 @@ export function Header(): ReactNode {
                         <h1>🐶🐱</h1>
                     </Link>
                 </div>
-                <div className={styles.linkList} style={{textDecoration: "none"}}>
+                
+                {/* デスクトップメニュー */}
+                <div 
+                    className={styles.linkList} 
+                    style={{
+                        textDecoration: "none",
+                        display: showDesktopMenu ? "flex" : "none"
+                    }}
+                >
                     <Link to="/">Home</Link>
                     <Link to="/about">About</Link>
                     <Link to="/blog">Blog</Link>
                     <Link to="/works">Works</Link>
                 </div>
+                
+                {/* モバイルハンバーガーボタン */}
                 <div 
-                    className={styles.hamburgerIcon} 
+                    className={styles.hamburgerIcon}
+                    style={{
+                        display: showMobileButton ? "flex" : "none"
+                    }}
                     onClick={toggleMenu} 
                     role="button" 
                     tabIndex={0} 
